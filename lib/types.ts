4aeef380140b87fd0503eb00a9beaa7437031aec
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-04-20 18:15:07 +0800

    feat: Implement basic frontend to test CRUD operations

---
+++ lib/types.ts
@@ -0,0 +1,15 @@
+export type Pog = {
+  id?: number;
+  name: string;
+  ticker_symbol: string;
+  price: number;
+  color: string;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  isAdmin: boolean;
+  balance: number;
+};
