6657cccb11e622df6663bd220a346525bca4719a
jedmamosto <jedmamosto@gmail.com>
2024-04-23 16:42:53 +0800

    feat: ui mockups ~ no integration yet

---
+++ components/home/MarqueeTag.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MarqueeTag = () => {
+  return (
+    <div className="flex justify-start border border-gray-100 bg-slate-50 rounded mr-6">
+      <span className="px-1 bg-red-700 rounded-tl rounded-bl"></span>
+      <p className="px-2 text-sm text-gray-950 font-medium">POG</p>
+    </div>
+  )
+}
+
+export default MarqueeTag
