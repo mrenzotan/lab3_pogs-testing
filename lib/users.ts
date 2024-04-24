ce2363c00e46080cbe76be7ddf1e5a44e5dcbb07
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-04-24 17:55:13 +0800

    setup: prisma integration

--- lib/users.ts
+++ lib/users.ts
@@ -1,44 +1,40 @@
-import initDatabase from '@/db/database';
+import prisma from '@/lib/prisma';
 
-const db = initDatabase();
+// export const createUser = async (user: {
+//   name: string;
+//   email: string;
+//   isAdmin?: boolean;
+//   balance?: number;
+// }) => {
+//   const { name, email, isAdmin = false, balance = 0 } = user;
+//   const createdUser = await prisma.user.create({
+//     data: { name, email, isAdmin, balance },
+//   });
+//   return createdUser;
+// };
 
-export const createUser = (user: {
-  name: string;
-  email: string;
-  password: string;
-  isAdmin?: boolean;
-  balance?: number;
-}) => {
-  const { name, email, password, isAdmin = false, balance = 0 } = user;
-  const stmt = db.prepare(
-    'INSERT INTO users (name, email, password, is_admin, balance) VALUES (?, ?, ?, ?, ?)'
-  );
-  const info = stmt.run(name, email, password, isAdmin, balance);
-  return { id: info.lastInsertRowid, name, email, isAdmin, balance };
-};
+// export const getUserByEmail = (email: string) => {
+//   const stmt = db.prepare('SELECT * FROM users WHERE email = ?');
+//   return stmt.get(email);
+// };
 
-export const getUserByEmail = (email: string) => {
-  const stmt = db.prepare('SELECT * FROM users WHERE email = ?');
-  return stmt.get(email);
-};
+// export const updateUser = (user: {
+//   id: number;
+//   name: string;
+//   email: string;
+//   password: string;
+//   isAdmin: boolean;
+//   balance: number;
+// }) => {
+//   const { id, name, email, password, isAdmin, balance } = user;
+//   const stmt = db.prepare(
+//     'UPDATE users SET name = ?, email = ?, password = ?, is_admin = ?, balance = ? WHERE id = ?'
+//   );
+//   stmt.run(name, email, password, isAdmin, balance, id);
+//   return user;
+// };
 
-export const updateUser = (user: {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-  isAdmin: boolean;
-  balance: number;
-}) => {
-  const { id, name, email, password, isAdmin, balance } = user;
-  const stmt = db.prepare(
-    'UPDATE users SET name = ?, email = ?, password = ?, is_admin = ?, balance = ? WHERE id = ?'
-  );
-  stmt.run(name, email, password, isAdmin, balance, id);
-  return user;
-};
-
-export const deleteUser = (id: number) => {
-  const stmt = db.prepare('DELETE FROM users WHERE id = ?');
-  stmt.run(id);
-};
+// export const deleteUser = (id: number) => {
+//   const stmt = db.prepare('DELETE FROM users WHERE id = ?');
+//   stmt.run(id);
+// };
