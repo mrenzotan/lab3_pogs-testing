25c6e9df3256a15542032c320592a673232ceafc
jedmamosto <jedmamosto@gmail.com>
2024-04-25 21:44:15 +0800

    fix: weird

--- lib/pogs.ts
+++ lib/pogs.ts
@@ -2,11 +2,11 @@ import prisma from '@/lib/prisma';
 import { Pog } from './types';
 
 export const readPogs = async () => {
-  return await prisma.pog.findMany();
+  return await prisma.pogs.findMany();
 };
 
 export const readSpecificPog = async (id: number) => {
-  return await prisma.pog.findUnique({
+  return await prisma.pogs.findUnique({
     where: {
       id: id
     }
@@ -15,7 +15,7 @@ export const readSpecificPog = async (id: number) => {
 
 export const createPog = async (pog: Pog) => {
   const { name, ticker_symbol, price, color } = pog;
-  const createdPog = await prisma.pog.create({
+  const createdPog = await prisma.pogs.create({
     data: { name, ticker_symbol, price, color },
   });
   return createdPog;
